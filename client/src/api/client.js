4bdbd9720960616e51dd505fb4351b9338054cb8
I'm Afk <132127314+ImAfksite@users.noreply.github.com>
2026-09-08 15:18:14 -0700

    Create client.js

---
+++ client/src/api/client.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 10000,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('crowdchat_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('crowdchat_token');
+      localStorage.removeItem('crowdchat_user');
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
